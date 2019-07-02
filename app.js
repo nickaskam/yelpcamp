c63b4bd6fbad0567d9d84ad3cc63c46fce2e08c8
Nick Askam <asknickfilms@gmail.com>
2019-07-01 20:54:13 -0500

    adding new form page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -21,6 +23,10 @@ app.post("/campgrounds", function(req, res){
 	res.send("You hit the post route!")
 	// get data from form and add to campgrounds array
 	//redirect back to campgrounds page
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
