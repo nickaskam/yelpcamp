5ad38bf8e53363008f424d117bfe64fb4e6db193
Nick Askam <asknickfilms@gmail.com>
2019-07-01 20:45:30 -0500

    adding the post route

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ app.get("/campgrounds", function(req, res){
 	res.render("campgrounds",{campgrounds:campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+	res.send("You hit the post route!")
+	// get data from form and add to campgrounds array
+	//redirect back to campgrounds page
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("the yelpcamp server has started");
 });
