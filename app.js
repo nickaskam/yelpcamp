bd3f7714f6ca335665b15e1d81be2fb63f423af2
Nick Askam <asknickfilms@gmail.com>
2019-07-01 21:00:10 -0500

    creating link to form and array

--- app.js
+++ app.js
@@ -5,24 +5,29 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+	{name: "Grapevine Lake", image: "https://media.nbcdfw.com/images/652*367/Lake-Grapevine.jpg"},
+	{name: "Broken Bow", image: "https://upload.wikimedia.org/wikipedia/en/b/bd/Broken_bow_lake.jpg"},
+	{name: "White Rock Lake", image: "http://mediad.publicbroadcasting.net/p/kera/files/styles/x_large/public/201806/shutterstock_753593347.jpg"}
+];
+
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Grapevine Lake", image: "https://media.nbcdfw.com/images/652*367/Lake-Grapevine.jpg"},
-		{name: "Broken Bow", image: "https://upload.wikimedia.org/wikipedia/en/b/bd/Broken_bow_lake.jpg"},
-		{name: "White Rock Lake", image: "http://mediad.publicbroadcasting.net/p/kera/files/styles/x_large/public/201806/shutterstock_753593347.jpg"}
-	]
-
 	res.render("campgrounds",{campgrounds:campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
 	res.send("You hit the post route!")
 	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image:image};
+	campgrounds.push(newCampground);
 	//redirect back to campgrounds page
+	res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res){
