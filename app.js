994194f566ce3573cda87a256576a81d46b8b671
Nick Askam <asknickfilms@gmail.com>
2019-08-21 22:42:17 -0500

    adding post route for comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express    = require("express"),
 	bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
 	Campground = require("./models/campground"),
+	Comment    = require(".models/comment"),
 	seedDB     = require("./seeds")
 
 seedDB();
@@ -81,6 +82,25 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 			res.render("comments/new", {campground: campground});
 		}
 	})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			})
+		}
+	}
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
